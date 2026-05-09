3b55acf712b8d2f5efcce75a4659a6effa102a33
IlvieRath <ilvierath0408@gmail.com>
2026-05-09 19:47:11 +0200

    First look

--- votey-website/app/page.tsx
+++ votey-website/app/page.tsx
@@ -3,13 +3,19 @@ import Image from "next/image";
 export default function Home() {
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl leading-10 tracking-tight text-black font-bagel">
-            To get started, edit the page.tsx file.
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full flex-col items-center justify-between py-32 bg-[#FFFEF5]">
+          
+          <div className="flex flex-col items-center gap-8">
+          <div className="flex items-center justify-center ml-42 mr-42">
+            <img src="Votey.png" alt="Votey Logo" />
+          </div>
+
+          <h1 className="text-center text-8xl leading-10 tracking-tight font-bagel text-[#F77646]">
+            Votey
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600">
+          </div>
+          <p className="text-lg leading-8">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -26,7 +32,7 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
-        </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] md:w-[158px]"
