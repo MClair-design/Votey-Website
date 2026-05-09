11e15a89413252f59b867fb90d6aa89f514729c9
IlvieRath <ilvierath0408@gmail.com>
2026-05-09 18:45:08 +0200

    implement fonts

--- votey-website/app/page.tsx
+++ votey-website/app/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
       <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black ">
+          <h1 className="max-w-xs text-3xl leading-10 tracking-tight text-black font-bagel">
             To get started, edit the page.tsx file.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 ">
+          <p className="max-w-md text-lg leading-8 text-zinc-600">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
