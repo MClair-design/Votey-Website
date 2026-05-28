3611b216f37178999aa08daa7e7a320b2a9edad5
IlvieRath <ilvierath0408@gmail.com>
2026-05-28 15:50:18 +0200

    vercel

--- votey-website/app/layout.tsx
+++ votey-website/app/layout.tsx
@@ -1,10 +1,16 @@
 import "./globals.css";
 
-export default function RootLayout({ children }) {
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
 }
+
 
